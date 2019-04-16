7a93050384505f14dd23af29659b4fca97963272
Daniel Shroff <danielshroff@hotmail.com>
2019-04-16 17:17:14 +0530

    Added examples

---
+++ IronRADev/examples/prog.cs
@@ -0,0 +1,14 @@
+class Program
+{
+static void Main(string[] args)
+{
+string  dog  =  "hi";
+
+dg
+System.Console.Write( dog);
+"hi"
+System.Console.WriteLine( dog);
+"hiii"
+System.Console.WriteLine("hello");
+}
+}
